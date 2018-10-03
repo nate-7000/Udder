06b16cb95c66b68a2c1da5eae2f784d017393722
Nathan Abraham <nathanabraham@nathans-macbook-pro.local>
2018-10-02 23:58:42 -0400

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Nathan Abraham on 2018-10-02.
+//  Copyright © 2018 Nathan Abraham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
